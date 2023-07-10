00d15b5e119e5ad11e88e3277cf0ab6da3deb7da
Замыслова Светлана <zamyslova_svetlana_17-05@mail.ru>
2023-07-10 08:42:37 +0000

    Update metric.py

--- metric.py
+++ metric.py
@@ -21,13 +21,41 @@ class Metric:
                 except:
                     continue
             if len(metric_edge_dict)>0:
-                edge_metric.append(metric_edge_dict)
+                edge_metric.append([metric_edge_dict, combine_variables[i][1][0]])
 
         return edge_metric
 
     def join_metrics(self, data):
 
+        new_metric_edge_dict = {}
+        new_sum_e_dict = {}
+        new_total_count_dict = {}
+        #нормально переделать, добавить начальную точку
+        for i in range(1,4):#len(data)):
+            #print(data[i])
+
+            for k, v in data[i][0].items():
+                #print(k, v)
+                #print()
+                if k in new_metric_edge_dict:
+                    #умножить каждый переход на индивидуальное количество -> все сложить -> разделить на общее количество
+                    new_metric_edge_dict[k] = v
+                    new_sum_e_dict[k] = new_sum_e_dict[k] + v * data[i][1]
+                    new_total_count_dict[k] = new_total_count_dict[k] + data[i][1]
+                else:
+                    new_metric_edge_dict[k] = v
+                    new_sum_e_dict[k] = v * data[i][1]
+                    new_total_count_dict[k] = data[i][1]
+
+            for i in new_metric_edge_dict:
+                new_metric_edge_dict[i] = new_sum_e_dict[i]/new_total_count_dict[i]
+
+        for k, v in new_metric_edge_dict.items():
+            print(k, v)
+
         return 0
+
+
 
 
 
