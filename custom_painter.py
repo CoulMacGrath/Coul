5a60f12b00bb6c6cd418c7798b3ae420ccd562e0
moria <moria1war1@gmail.com>
2023-07-13 13:46:10 +0700

    Custom painter (class rework) v1.01 Main_old (test Custom_painter)

--- custom_painter.py
+++ custom_painter.py
@@ -1,25 +1,44 @@
 import graphviz as gz
 import logging
+import base64
+
+
 logging.basicConfig(level=20, filename="custom_painter_log.log",
                             format="%(asctime)s %(levelname)s %(message)s")
 
+
 class CustomPainter():
 
-    def create(self,
-               nodes,
-               edges,
-               file_name,
-               format='svg',
-               node_style_metric={'shape': 'plaintext', 'color': '#2d137d', 'fontcolor': '#2d137d',
-                                         'fontsize': '12.0', 'size': '2', 'image': '1.png'},
-               edge_style_metric={'color': '#2d137d', 'fontcolor': '#2d137d', 'fontsize': '9.0'}):
+    def __init__(self, nodes=None, edges=None,  logfile=None,
+                 node_style_metric=None, edges_style_metric=None,
+                 theme=None, format=None, base64image=None):
+        self.nodes = None
+        self.edges = None
+        self.file_name = None
+        self.format = 'svg'
+        self.node_style_metric = {'shape': 'plaintext', 'color': '#2d137d', 'fontcolor': '#2d137d',
+                                         'fontsize': '12.0', 'size': '2', 'image': '1.png'}
+        self.edges_style_metric= {'color': '#2d137d', 'fontcolor': '#2d137d', 'fontsize': '9.0'}
+        self.theme = None
+        self.logfile = None
+        self.base64image = None
 
 
-        ps = gz.Digraph(file_name, node_attr=node_style_metric, edge_attr=edge_style_metric)
+
+
+    def create(self, nodes, edges, file_name):
+        self.nodes = nodes
+        self.edges = edges
+        self.file_name = file_name
+
+        #logging.basicConfig(level=self.logfile.level, filename=self.logfile.filename,
+        #                    format=self.logfile.format)
+
+        ps = gz.Digraph(self.file_name, node_attr=self.node_style_metric, edge_attr=self.edges_style_metric)
 
         try:
-            for g_node in nodes:
-                metric = nodes.get(g_node).metrics.get('count')
+            for g_node in self.nodes:
+                metric = self.nodes.get(g_node).metrics.get('count')
                 if g_node == 'startevent':
                     ps.node(g_node, image='', label='')
                 elif g_node == 'endevent':
@@ -30,8 +49,8 @@ class CustomPainter():
             logging.error(f"nodes metrics error")
 
         try:
-            for g_edge in edges:
-                metric = edges.get(g_edge).metrics.get('count')
+            for g_edge in self.edges:
+                metric = self.edges.get(g_edge).metrics.get('count')
                 if metric == None:
                     ps.edge(g_edge[0], g_edge[1])
                 else:
@@ -40,10 +59,19 @@ class CustomPainter():
             logging.error(f"edges metrics error")
 
 
-        ps.format = format
+        ps.format = self.format
         #cleanup=True - не сохранять файлы .gv
         ps.render(cleanup=True)
-        logging.info(f"call create: file_name - {file_name}, format - {format}")
+        with open(str(self.file_name)+".gv."+self.format, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+            image_file.close()
+            self.base64image = encoded_string
+
+
+        logging.info(f"call create: file_name - {self.file_name}, format - {self.format}")
+
+
+
 
 
 
--- main_old.py
+++ main_old.py
@@ -8,6 +8,7 @@ import clickhouse_connect
 #Graphviz
 from sberpm.visual import GraphvizPainter,ChartPainter
 import graphviz as gz
+import custom_painter as c_p
 
 #Различные варинаты майнеров
 from sberpm.miners import HeuMiner, SimpleMiner,CausalMiner,AlphaMiner,AlphaPlusMiner, InductiveMiner, CorrelationMiner
@@ -120,34 +121,35 @@ def  initializating_pm():
 
 #Кастомная функция для отрисовки готовых графов, создана по причине отсутствия стилизации средств встроенных в библиотеку sberPm
 def custom_graph(nodes,edges,file,format='svg'):
-
-    ps = gz.Digraph(file, node_attr={'shape': 'plaintext', 'color': '#2d137d', 'fontcolor': '#2d137d',
-                                     'fontsize': '12.0', 'size': '2', 'image': '1.png'},
-                                      edge_attr={'color': '#2d137d', 'fontcolor': '#2d137d'})
+    img = c_p.CustomPainter()
+    img.create(nodes,edges,file)
+    #ps = gz.Digraph(file, node_attr={'shape': 'plaintext', 'color': '#2d137d', 'fontcolor': '#2d137d',
+    #                                 'fontsize': '12.0', 'size': '2', 'image': '1.png'},
+    #                                  edge_attr={'color': '#2d137d', 'fontcolor': '#2d137d'})
 
  #Перенос вершин графов и их весов из майнера в Digraph
     #Перенос вершин графов
-    for g_node in nodes:
-        metric = nodes.get(g_node).metrics.get('count')
-        if g_node == 'startevent':
-            ps.node(g_node, image='', label='')
-        elif g_node == 'endevent':
-            ps.node(g_node, image='', label='')
-        else:
-            ps.node(g_node,label=r'' + g_node + '\n'+'             ' + str(metric) + '\l')
+    #for g_node in nodes:
+    #    metric = nodes.get(g_node).metrics.get('count')
+   #     if g_node == 'startevent':
+    #        ps.node(g_node, image='', label='')
+   #     elif g_node == 'endevent':
+    #        ps.node(g_node, image='', label='')
+     #   else:
+      #      ps.node(g_node,label=r'' + g_node + '\n'+'             ' + str(metric) + '\l')
 
     #Перенос весов графов
-    for g_edge in edges:
-        metric = edges.get(g_edge).metrics.get('count')
-        if metric == None:
-            ps.edge(g_edge[0],g_edge[1])
-        else:
-            ps.edge(g_edge[0],g_edge[1],label=str(metric))
+ #   for g_edge in edges:
+  #      metric = edges.get(g_edge).metrics.get('count')
+   #     if metric == None:
+    #        ps.edge(g_edge[0],g_edge[1])
+     #   else:
+      #      ps.edge(g_edge[0],g_edge[1],label=str(metric))
 
 
     #Вывод svg файла
-    ps.format = format
-    ps.render()
+  #  ps.format = format
+   # ps.render()
 
 if __name__ == '__main__':
     initializating_pm()
